d3db51b54aad17012ca3741ffcc4f344518d46bf
haggis22 <dshell@gmail.com>
2020-07-12 16:37:37 -0500

    Creating initial Node project

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+// server.js
+var express = require("express");
+var socket = require("socket.io");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var app = express();
+
+var port = 3000;
+
+var server = app.listen(port, function () {
+    console.log("Listening at http://localhost: " + port);
+});
+
+app.use(express.static("public"));
+
+var sock = socket(server);
+
+sock.on("connection", function (socket) {
+    console.log("made connection with socket " + socket.id);
+    socket.on("chat", function (data) {
+        sock.sockets.emit("chat", data);
+    });
+
+    socket.on("typing", function (data) {
+        // send an event to everyone but the person who emitted the typing event to the server
+        socket.broadcast.emit("typing", data);
+    });
+});
