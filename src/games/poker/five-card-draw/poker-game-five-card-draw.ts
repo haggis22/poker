5e85c7e403818802b850b15068d4f03f185b1408
haggis22 <dshell@gmail.com>
2020-07-21 09:27:22 -0500

    added round of betting to 5-card-draw

--- src/games/poker/five-card-draw/poker-game-five-card-draw.ts
+++ src/games/poker/five-card-draw/poker-game-five-card-draw.ts
@@ -6,6 +6,7 @@ import { ShowdownAction } from "../../actions/showdown-action";
 import { Board } from "../../../casino/tables/boards/board";
 import { NoBoard } from "../../../casino/tables/boards/no-board";
 import { Best5InHandSelector } from "../../hand-selectors/best-5-in-hand-selector";
+import { BetAction } from "../../actions/betting/bet-action";
 
 export class PokerGameFiveCardDraw extends Game {
 
@@ -19,6 +20,8 @@ export class PokerGameFiveCardDraw extends Game {
         this.actions.push(new DealAction(false));
         this.actions.push(new DealAction(false));
         this.actions.push(new DealAction(false));
+
+        this.actions.push(new BetAction(BetAction.FIRST_POSITION));
 
         this.actions.push(new ShowdownAction());
 
