9524564e16a924bd0a7a2d6f3c80241029f87078
haggis22 <dshell@gmail.com>
2020-07-21 17:51:46 -0500

    Created IAction to descibe events on the table

---
+++ src/actions/deal-card-action.ts
@@ -0,0 +1,17 @@
+﻿import { IAction } from "./action";
+import { DealtCard } from "../hands/dealt-card";
+
+export class DealCardAction implements IAction {
+
+    public playerID: number;
+    public card: DealtCard;
+
+    constructor(playerID: number, card: DealtCard) {
+
+        this.playerID = playerID;
+        this.card = card;
+
+    }
+
+
+}
