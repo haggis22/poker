6e38fdc4e2f4fd337fb052300be14866a027d267
haggis22 <dshell@gmail.com>
2020-07-30 22:17:45 -0500

    Moved AddChipsAction to players actions; new StackUpdateAction

---
+++ src/actions/players/stack-update-action.ts
@@ -0,0 +1,16 @@
+﻿import { Action } from "../action";
+
+export class StackUpdateAction implements Action {
+
+    public tableID: number;
+    public playerID: number;
+    public chips: number;
+
+
+    constructor(tableID: number, playerID: number, chips: number) {
+        this.tableID = tableID;
+        this.playerID = playerID;
+        this.chips = chips;
+    }
+
+}
