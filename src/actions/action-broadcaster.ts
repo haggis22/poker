4c617ce5f3700a37438a122065aa2ec6cca50d3d
haggis22 <dshell@gmail.com>
2020-07-30 22:17:05 -0500

    Added broadcast method to interface

--- src/actions/action-broadcaster.ts
+++ src/actions/action-broadcaster.ts
@@ -1,10 +1,13 @@
 ﻿import { TableObserver } from "../casino/tables/table-observer";
+import { Action } from "./action";
 
 export interface ActionBroadcaster {
 
     register(observer: TableObserver);
 
     unregister(observer: TableObserver);
+
+    broadcast(action: Action);
 
 
 
