c0ea779fb3f1b0c82fbb19b6f9935fe9966ea3da
haggis22 <dshell@gmail.com>
2020-08-15 17:54:21 -0500

    Added more classes to serializable

--- src/communication/serializable.ts
+++ src/communication/serializable.ts
@@ -1,4 +1,27 @@
-﻿export { TableAction } from "../actions/table/table-action";
+﻿export { Pot } from "../casino/tables/betting/pot";
+export { Bet } from "../casino/tables/betting/bet";
+export { Fold } from "../casino/tables/betting/fold";
+export { FoldAction } from "../actions/table/betting/fold-action";
+export { FoldCommand } from "../commands/table/fold-command";
+export { BetCommand } from "../commands/table/bet-command";
+
+export { Card } from "../cards/card";
+export { CardValue } from "../cards/card-value";
+export { CardSuit } from "../cards/card-suit";
+
+export { TableStateMachine } from "../casino/tables/states/table-state-machine";
+export { BestHandSelector } from "../games/hand-selectors/best-hand-selector";
+export { HandEvaluator } from "../games/hand-evaluator";
+export { HandDescriber } from "../games/hand-describer";
+
+export { Game } from "../games/game";
+export { Stakes } from "../casino/tables/betting/stakes";
+export { TableRules } from "../casino/tables/table-rules";
+export { TableState } from "../casino/tables/states/table-state";
+export { Board } from "../casino/tables/boards/board";
+export { Deck } from "../cards/deck";
+export { BetTracker } from "../casino/tables/betting/bet-tracker";
+export { TableAction } from "../actions/table/table-action";
 export { Seat } from "../casino/tables/seat";
 export { Hand } from "../hands/hand";
 export { Table } from "../casino/tables/table";
@@ -8,7 +31,5 @@ export { Command } from "../commands/command";
 export { Message } from "../messages/message";
 export { ActionMessage } from "../messages/action-message";
 export { PlayerSeatedAction } from "../actions/table/players/player-seated-action";
-
-
 
 
