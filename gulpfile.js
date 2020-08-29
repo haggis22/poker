e933b6fe5b60549912b5d06a1597aed19efc983c
haggis22 <dshell@gmail.com>
2020-08-29 18:09:44 -0500

    HelloApp in angular javascript (not typescript yet)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 ﻿const gulp = require('gulp');
 const del = require('del');
+var concat = require('gulp-concat');
 
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
@@ -27,5 +28,18 @@ gulp.task('client-html', () => {
 
 });
 
+gulp.task('client-js', () => {
+
+    // combines all the Angular code from /src along with the dual Angular/Node code from /js
+    return gulp.src(['src/client/**/*.js'])
+        .pipe(concat('poker-js.js'))
+        .pipe(gulp.dest('build/client/js'));
+
+
+});
+
+gulp.task('client', gulp.parallel('client-html', 'client-js'));
+
+
 gulp.task('default',
-    gulp.series('clean', gulp.parallel('ts', 'client-html')));
+    gulp.series('clean', gulp.parallel('ts', 'client')));
---
+++ src/client/hello/hello-controller.js
@@ -0,0 +1,17 @@
+﻿(function(app) {
+
+    'use strict';
+
+    app.controller('HelloCtrl', ['$scope',
+
+        function($scope) {
+
+
+            $scope.name = 'Danny';
+        
+
+        }  // outer function
+
+    ]);
+
+})(angular.module('HelloApp'));
