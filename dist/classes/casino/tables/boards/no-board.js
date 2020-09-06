2f16a72208f40606367aebe5c04b8c93c5d228f0
haggis22 <dshell@gmail.com>
2020-09-05 19:37:30 -0500

    ignoring dist folder

--- dist/classes/casino/tables/boards/no-board.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NoBoard = void 0;
-class NoBoard {
-}
-exports.NoBoard = NoBoard;
--- dist/classes/commands/command.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Command = void 0;
-class Command {
-    constructor() {
-    }
-}
-exports.Command = Command;
