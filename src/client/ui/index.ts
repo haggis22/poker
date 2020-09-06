d3bece326bdb43cfc1bdf2c707fe91237aaf01f9
haggis22 <dshell@gmail.com>
2020-09-05 20:29:43 -0500

    First version that actually uses the REAL Player typescript file

--- src/client/ui/index.ts
+++ src/client/ui/index.ts
@@ -1,20 +1,37 @@
 ﻿import './style.scss';
+import { Player } from '../../players/player';
 
 import Vue from 'vue';
+
+let playerID: number = 0;
+
+let players: Player[] = new Array<Player>();
+
+players.push(createPlayer('Danny', 1000));
+players.push(createPlayer('Mark', 400));
+players.push(createPlayer('Paul', 600));
+players.push(createPlayer('Joe', 800));
+players.push(createPlayer('Sekhar', 0));
 
 var app = new Vue({
     el: '#poker',
     data: {
         name: 'Daniel',
         message: 'Here we go again',
-        players:
-            [
-                { name: 'Daniel', chips: 500, playing: false },
-                { name: 'Mark', chips: 200, playing: false }
-            ]
-
+        players: players
 
     }
 });
 
 console.log('Here I am');
+
+
+
+function createPlayer(name: string, numChips: number): Player {
+
+    let player: Player = new Player(++playerID, name);
+    player.chips = numChips;
+
+    return player;
+
+}
