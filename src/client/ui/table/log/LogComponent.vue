f9cf48abaad7e3d611659a8585ad9472e8abc146
haggis22 <dshell@gmail.com>
2020-12-27 15:33:00 -0600

    Added src/client/ui/table/log/.. to source control

---
+++ src/client/ui/table/log/LogComponent.vue
@@ -0,0 +1,57 @@
+﻿<template>
+
+    <div class="action-log">
+
+        <div v-for="(message, index) in ui.messages" 
+             :key="`log-${index}`"
+             class="message">{{ message }}</div>
+
+    </div>
+
+</template>
+
+
+<script lang="ts">
+
+
+import './log.scss';
+
+import Vue from 'vue';
+
+import { TableUI } from '../../../table-ui';
+
+const LogComponent = Vue.extend ({
+
+    props: {
+        ui: {
+            type: TableUI,
+            required: true
+        }
+    },
+    methods: {
+
+
+        scrollToEnd() {
+
+            const messages = this.$el.getElementsByClassName('message');
+
+            if (messages.length) {
+
+                messages[messages.length - 1].scrollIntoView();
+
+            }
+
+
+        }
+    },
+    updated() {
+
+        this.scrollToEnd();
+
+    }
+
+});
+
+export default LogComponent;
+
+</script>
